f83f264fc8d767be237e641bb3a9d5b315f132be
Lonchi78 <andrejloncik@gmail.com>
2018-06-03 15:00:20 +0200

    Add Common class

--- app/src/main/java/com/lonchi/andrej/weatherapp/Common/Common.java
+++ app/src/main/java/com/lonchi/andrej/weatherapp/Common/Common.java
@@ -13,9 +13,11 @@ import java.util.Date;
  */
 
 public class Common {
+    //  Unicate variables of my account on API site
     public static String API_KEY = "a8444bdb6a3d66c63173043e098776cf";
     public static String API_LINK = "http://api.openweathermap.org/data/2.5/weather";
 
+    //  Method send an API Request by coordinations of location
     @NonNull
     public static String apiRequest(String lat, String lng ){
         StringBuilder sb = new StringBuilder( API_LINK );
@@ -23,6 +25,7 @@ public class Common {
         return sb.toString();
     }
 
+    //  Convert Unix Time Stamp to normal Date Time
     public static String unixTimeStampToDateTime( double unixTimeStamp ){
         DateFormat mDateFormat = new SimpleDateFormat( "HH:mm" );
         Date mDate = new Date();
@@ -30,10 +33,12 @@ public class Common {
         return mDateFormat.format( mDate );
     }
 
+    //  Get Image of current weather from web
     public static String getImage( String icon ){
         return String.format( "http://openweathermap.org/img/w/%s.png", icon );
     }
 
+    //  Get current date and time
     public static String getDateNow() {
         DateFormat mDateFormat = new SimpleDateFormat( "dd MMMM yyyy HH:mm" );
         Date now = new Date();
